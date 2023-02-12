b4c3afff3dbbf41a5edb75df63df875d21533669
Camilla Knutsen <camilla.knutsen@bouvet.no>
2023-02-12 21:18:36 +0100

    formatting

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,34 +2,35 @@ import React from "react";
 import styles from "./Header.module.css";
 
 function Header({ portalList, changePortal, portal, contentRef }) {
-  
   const firstImg = portal?.cover?.url;
   const [heading, setHeading] = React.useState("Nå");
   const [portalImg, setPortalImg] = React.useState(firstImg);
 
   return (
-    <header  className={styles.header}>
+    <header className={styles.header}>
       <h1 className={styles.title}>Nyhetsmeldinger</h1>
       <nav aria-label="portals" className={styles.wrapper}>
         {portalList.map((portal) => (
-          <button style={portal.cover.url && {backgroundImage: `url(${portal?.cover?.url})`}}
+          <button
+            style={
+              portal.cover.url && {
+                backgroundImage: `url(${portal?.cover?.url})`,
+              }
+            }
             className={styles.button}
             key={Math.random()}
             onClick={() => {
               changePortal(portal);
               setHeading(portal.name);
               setPortalImg(portal.cover.url);
-            //   contentRef.current.scrollIntoView();
-
+              //   contentRef.current.scrollIntoView();
             }}
           >
             <span>{portal.name}</span>
           </button>
         ))}
-        
       </nav>
       <h1 className={styles.heading}>{heading}</h1>
-      
     </header>
   );
 }
